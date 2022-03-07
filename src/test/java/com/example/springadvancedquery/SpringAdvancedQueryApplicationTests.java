c12f0d559f8230cff18f48187670fb4b8d0f2c09
Dimo Yanev <dimocz@gmail.com>
2022-03-07 19:52:26 +0100

    Initial commit

---
+++ src/test/java/com/example/springadvancedquery/SpringAdvancedQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springadvancedquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
